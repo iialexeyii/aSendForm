b62cf085c103530ffef30f26954c4eae6a820e6d
alexeyh <aleks300395@mail.ru>
2015-05-28 13:17:38 +0300

    ver 1.9.1 Refrect and popupTriger

--- gruntfile.js
+++ gruntfile.js
@@ -7,9 +7,7 @@ module.exports = function(grunt) {
 		uglify: {
 			js: {
 				options: {
-					sourceMap: true,
-					sourceMapName: 'assets/jquery.send.form.js.map',
-					sourceMapIncludeSources: true
+
 				},
 				files: {
 					'assets/jquery.send.form.min.js': ['assets/jquery.send.form.js']
